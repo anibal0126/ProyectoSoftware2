1b936921073bca98220acfb683adce9760f434d0
AnibalMuñoz <AnibalMuoz@192.168.1.13>
2020-10-26 16:21:56 -0500

    Login

---
+++ src/java/modelo/LoginBean.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author anibalMuñoz
+ */
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+    
+    Connection connect = null;
+     
+     Conexion conexion= new Conexion();
+    
+     public int cedula, contraseña;
+     
+     private boolean autenticado;
+    
+    public void ingresar() throws ClassNotFoundException, SQLException, IOException{
+        
+     System.out.println(cedula);
+     System.out.println(contraseña);
+     
+     connect=conexion.conectar();
+     
+     String consulta = "select cedula, contraseña from usuario where cedula='"+cedula+"' AND contraseña='"+contraseña+"'" ;
+
+        PreparedStatement pstmt = connect.prepareStatement(consulta);
+        ResultSet rs = pstmt.executeQuery();
+        
+     if(rs.next()){
+         
+     FacesContext.getCurrentInstance().getExternalContext().redirect("listaCasasRurales.xhtml");
+     autenticado = true;
+         
+     }else{
+         
+         autenticado = false;
+     }
+     
+ }
+
+    public Connection getConnect() {
+        return connect;
+    }
+
+    public void setConnect(Connection connect) {
+        this.connect = connect;
+    }
+
+    public Conexion getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Conexion conexion) {
+        this.conexion = conexion;
+    }
+
+    public boolean isAutenticado() {
+        return autenticado;
+    }
+
+    public void setAutenticado(boolean autenticado) {
+        this.autenticado = autenticado;
+    }
+    
+    
+   
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(int contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+    
+}
